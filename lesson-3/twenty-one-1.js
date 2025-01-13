169fcfee04049d7fb0e6356d2cb7e0f2866cf1c2
Zack Brashear <zackbrashear@gmail.com>
2025-01-13 11:21:15 -0500

    fix a dealer double prompt for twenty-one.js

--- lesson-3/twenty-one-1.js
+++ lesson-3/twenty-one-1.js
@@ -9,20 +9,6 @@ function prompt(msg) {
   console.log("=> " + msg);
 }
 
-function joinAnd(arr, punctuation = ", ", word = "and") {
-  let outputStr = "";
-  if (arr.length === 1) return String(arr[0]);
-  if (arr.length === 2) return `${String(arr[0])} ${word} ${String(arr[1])}`;
-  arr.forEach((el, idx) => {
-   if (idx !== arr.length - 1) {
-     outputStr += String(arr[idx]) + punctuation;
-   } else if (idx === arr.length - 1) {
-     outputStr += word + " " + String(arr[idx]);
-   }
-  })
-  return outputStr;
-}
-
 function returnSuit(card) {
   switch (card[0]) {
     case 'H': return 'Hearts';
@@ -47,7 +33,7 @@ function listCards(cards) {
   for (let card of cards) {
     readableCards.push(returnValue(card) + " of " + returnSuit(card));
   }
-  return joinAnd(readableCards)
+  return readableCards.join(", ")
 }
 
 function shuffle(array) {
@@ -100,6 +86,13 @@ function drawFirstCards(deck, playerHand, dealerHand) {
   drawCard(deck, dealerHand);
 }
 
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function simulateDelay() {
+  await delay(2000);
+}
 
 function topOfScreenDisplay(playerHand, dealerHand, playerChips, wager, playerTotal) {
   console.clear();
@@ -187,12 +180,13 @@ while (true) {
 
     while ((dealerTotal < DEALER_LIMIT) && !playerBust) {
       prompt(`Dealer has: ${listCards(dealerHand)}...`);
+      simulateDelay();
       prompt("Dealer hits!");
       drawCard(deck, dealerHand)
       dealerTotal = calculateHand(dealerHand);
 
+      prompt(`Dealer has: ${listCards(dealerHand)}.`)
       if (dealerTotal > TWENTY_ONE) {
-        prompt(`Dealer has: ${listCards(dealerHand)}.`)
         prompt('Dealer busts! You win!\n')
         dealerBust = true;
         playerChips += wager;
@@ -204,6 +198,7 @@ while (true) {
     // display winner for non-bust
     if (!dealerBust && !playerBust) {
       prompt(`Dealer has: ${listCards(dealerHand)}...`);
+      simulateDelay();
       prompt("Dealer Stays!\n")
       prompt(`Your total is ${playerTotal}.`);
       prompt(`Dealer total is ${dealerTotal}.`);
